468e7f15b7acfab4c95994f7c98c4336d14d869a
Akshay Joshi <akshay.joshi.m@gmail.com>
2024-09-30 12:36:51 +0200

    Update macOS_Brew_Packages_Install.sh

--- macOS_Brew_Packages_Install.sh
+++ macOS_Brew_Packages_Install.sh
@@ -26,17 +26,35 @@ else
     echo "Homebrew is already installed."
 fi
 
-# 3. Install specified packages
+# 3. Install specified packages if not already installed
 echo "Installing specified packages..."
-brew install python@3.12 aria2 dos2unix gh libunistring pypy3.10 tcl-tk wget \
-    ca-certificates gdbm libidn2 ncurses readline tree xz dmg2img \
-    gettext libssh2 openssl@3 sqlite watch robot-framework libpst
+packages=("python@3.12" "aria2" "dos2unix" "gh" "libunistring" "pypy3.10" "tcl-tk" "wget" \
+    "ca-certificates" "gdbm" "libidn2" "ncurses" "readline" "tree" "xz" "dmg2img" \
+    "gettext" "libssh2" "openssl@3" "sqlite" "watch" "robot-framework" "libpst")
 
-# 4. Install specified casks
+for package in "${packages[@]}"; do
+    if brew list --formula | grep -q "^${package}$"; then
+        echo "Package $package is already installed."
+    else
+        echo "Installing $package..."
+        brew install "$package"
+    fi
+done
+
+# 4. Install specified casks if not already installed
 echo "Installing specified casks..."
-brew install --cask adobe-acrobat-reader gimp gstreamer-development \
-    gstreamer-runtime podman-desktop utm microsoft-azure-storage-explorer \
-    visual-studio-code coteditor pycharm-ce chatgpt pgadmin4 \
-    omnidisksweeper disk-drill audacity logi-options+ vlc
+casks=("adobe-acrobat-reader" "gimp" "gstreamer-development" "gstreamer-runtime" \
+    "podman-desktop" "utm" "microsoft-azure-storage-explorer" "visual-studio-code" \
+    "coteditor" "pycharm-ce" "chatgpt" "pgadmin4" "omnidisksweeper" "disk-drill" \
+    "audacity" "logi-options+" "vlc")
+
+for cask in "${casks[@]}"; do
+    if brew list --cask | grep -q "^${cask}$"; then
+        echo "Cask $cask is already installed."
+    else
+        echo "Installing $cask..."
+        brew install --cask "$cask"
+    fi
+done
 
 echo "All specified packages and casks have been installed!"
